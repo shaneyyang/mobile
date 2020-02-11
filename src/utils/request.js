41873ed57d3b38b771aa348444f380ba311680b5
shane <2206907681@qq.com>
2020-02-11 18:19:05 +0800

    登录页面搭建

--- src/utils/request.js
+++ src/utils/request.js
@@ -42,18 +42,30 @@ instance.interceptors.request.use(function (config) {
 })
 
 // 配置【响应拦截器】
-instance.interceptors.response.use(function (response) {
-  //  得到的response实际上被axios包 一层数据
+axios.interceptors.response.use(function (response) {
+  // 正常响应处理
+  // 有时返回data、有时返回data.data
   try {
-    // 返回是response.data.data
-    //  将数据解构
+    // data.data如果报错，没有获得到，错误信息会被catch步骤，就走data了
     return response.data.data
-  } catch (error) {
-    // 返回是response.data
+  } catch (err) {
     return response.data
   }
 }, function (error) {
+  // 非正常响应处理(包括401)
+  // console.dir(error) // 对象： config request response isAxiosError toJSON
+  if (error.response.status === 401) {
+    // token不ok(token在服务器端已经失效了，2个小时时效)
+    // 强制用户重新登录系统，以刷新服务器端的token时效
+    router.push('/login')
+    // 不要给做错误提示了
+    return new Promise(function () {}) // 空的Promise对象，没有机会执行catch，进而不做错误提示了
+  }
+  // return new Promise((resolve,reject)=>{
+  // reject('获得文章失败！')
+  // })
   return Promise.reject(error)
 })
+
 // es6默认导出
 export default instance
--- src/views/user/login.vue
+++ src/views/user/login.vue
@@ -1,13 +1,48 @@
 <template>
-  <div class="page-user-chat">
-    <van-nav-bar left-arrow @click-left="$router.back()" title="登录"></van-nav-bar>
+  <div class="user-login">
+    <van-nav-bar title="登录"></van-nav-bar>
+      <van-cell-group>
+        <van-field
+          label="手机号"
+          v-model="loginForm.mobile"
+          type="text"
+          placeholder="请输入用户名"
+          required
+          clearable
+        />
+        <van-field
+          v-model="loginForm.code"
+          type="password"
+          placeholder="请输入密码"
+          label="密码"
+          required
+          clearable
+        >
+          <van-button slot="button" size="small" type="primary">发送验证码</van-button>
+        </van-field>
+      </van-cell-group>
+    <div class="login-btn">
+      <van-button type="info" block round size="small">登录</van-button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'user-chat'
+  name: 'user-login',
+  data () {
+    return {
+      loginForm: {
+        mobile: '',
+        code: ''
+      }
+    }
+  }
 }
 </script>
 
-<style scoped lang='less'></style>
+<style scoped lang='less'>
+.login-btn {
+  padding: 40px 0;
+}
+</style>
