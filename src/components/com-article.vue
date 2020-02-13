ae8061d0a391479ea8031f984aca78e110824f71
shane <2206907681@qq.com>
2020-02-13 12:01:26 +0800

    初步实现下拉刷新

--- src/components/com-article.vue
+++ src/components/com-article.vue
@@ -1,13 +1,17 @@
 <template>
-  <!-- 瀑布 -->
   <div class="scroll-wrapper">
-  <van-list v-model="loading" :finished="finished" finished-text="没有更多了" @load="onLoad">
-    <van-cell v-for="item in list" :key="item" :title="item" />
-  </van-list>
+    <!-- 下拉刷新 -->
+    <van-pull-refresh v-model="isLoading" @refresh="onRefresh">
+      <!-- 瀑布 -->
+      <van-list v-model="loading" :finished="finished" finished-text="没有更多了" @load="onLoad">
+        <van-cell v-for="item in list" :key="item" :title="item" />
+      </van-list>
+    </van-pull-refresh>
   </div>
 </template>
 
 <script>
+import { Toast } from 'vant'
 export default {
   name: 'com-article',
   data () {
@@ -15,10 +19,21 @@ export default {
       // 瀑布流成员
       list: [], // 接收加载好的数据
       loading: false, // 加载中动画
-      finished: false // 是否停止加载
+      finished: false, // 是否停止加载
+      // 下拉刷新成员
+      isLoading: false
     }
   },
   methods: {
+    onRefresh () {
+      setTimeout(() => {
+        Toast('刷新成功')
+        this.isLoading = false
+        // 调用瀑布流
+        this.onLoad()
+      }, 1000)
+    },
+
     // 瀑布流加载方法
     onLoad () {
       // 异步更新数据
