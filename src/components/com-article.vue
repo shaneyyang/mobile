e59567669b73cabb08a6137fa50cde4280a75ae0
shane <2206907681@qq.com>
2020-02-13 11:40:19 +0800

    组件独立

---
+++ src/components/com-article.vue
@@ -0,0 +1,51 @@
+<template>
+  <!-- 瀑布 -->
+  <div class="scroll-wrapper">
+  <van-list v-model="loading" :finished="finished" finished-text="没有更多了" @load="onLoad">
+    <van-cell v-for="item in list" :key="item" :title="item" />
+  </van-list>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'com-article',
+  data () {
+    return {
+      // 瀑布流成员
+      list: [], // 接收加载好的数据
+      loading: false, // 加载中动画
+      finished: false // 是否停止加载
+    }
+  },
+  methods: {
+    // 瀑布流加载方法
+    onLoad () {
+      // 异步更新数据
+      // setTimeout 仅做示例，真实场景中一般为 ajax 请求
+      setTimeout(() => {
+        for (let i = 0; i < 10; i++) {
+          this.list.push(this.list.length + 1)
+        }
+
+        // 加载状态结束
+        this.loading = false
+
+        // 数据全部加载完成
+        if (this.list.length >= 40) {
+          this.finished = true
+        }
+      }, 1000)
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+// 给上拉列表设置样式
+.scroll-wrapper {
+  height: 100%;
+  // y方向溢出设置
+  overflow-y: auto;
+}
+</style>
--- src/views/home/hindex.vue
+++ src/views/home/hindex.vue
@@ -4,11 +4,12 @@
       <van-tab title="推荐">
         推荐内容展示
         <!-- 瀑布流加载列表 -->
-        <div class="scroll-wrapper">
-        <van-list v-model="loading" :finished="finished" finished-text="没有更多了" @load="onLoad">
+
+        <!-- <van-list v-model="loading" :finished="finished" finished-text="没有更多了" @load="onLoad">
           <van-cell v-for="item in list" :key="item" :title="item" />
-        </van-list>
-        </div>
+        </van-list> -->
+        <com-article></com-article>
+
       </van-tab>
       <van-tab title="数据库">数据库内容展示</van-tab>
       <van-tab title="后端">后端内容展示</van-tab>
@@ -17,37 +18,41 @@
 </template>
 
 <script>
+import ComArticle from '../../components/com-article'
 export default {
   name: 'home-index',
   data () {
     return {
       // 激活频道下标
-      activeChannelIndex: 0,
-      // 瀑布流成员
-      list: [], // 接收加载好的数据
-      loading: false, // 加载中动画
-      finished: false // 是否停止加载
+      activeChannelIndex: 0
+      // // 瀑布流成员
+      // list: [], // 接收加载好的数据
+      // loading: false, // 加载中动画
+      // finished: false // 是否停止加载
     }
   },
+  components: {
+    ComArticle
+  },
   methods: {
-    // 瀑布流加载方法
-    onLoad () {
-      // 异步更新数据
-      // setTimeout 仅做示例，真实场景中一般为 ajax 请求
-      setTimeout(() => {
-        for (let i = 0; i < 10; i++) {
-          this.list.push(this.list.length + 1)
-        }
+    // // 瀑布流加载方法
+    // onLoad () {
+    //   // 异步更新数据
+    //   // setTimeout 仅做示例，真实场景中一般为 ajax 请求
+    //   setTimeout(() => {
+    //     for (let i = 0; i < 10; i++) {
+    //       this.list.push(this.list.length + 1)
+    //     }
 
-        // 加载状态结束
-        this.loading = false
+    //     // 加载状态结束
+    //     this.loading = false
 
-        // 数据全部加载完成
-        if (this.list.length >= 40) {
-          this.finished = true
-        }
-      }, 1000)
-    }
+    //     // 数据全部加载完成
+    //     if (this.list.length >= 40) {
+    //       this.finished = true
+    //     }
+    //   }, 1000)
+    // }
   }
 }
 </script>
@@ -68,12 +73,12 @@ export default {
   // 深度选择器
   /deep/ .van-tab__pane{
     height: 100%;
-    // 给上拉列表设置样式
-    .scroll-wrapper {
-      height: 100%;
-      // y方向溢出设置
-      overflow-y: auto;
-    }
+    // // 给上拉列表设置样式
+    // .scroll-wrapper {
+    //   height: 100%;
+    //   // y方向溢出设置
+    //   overflow-y: auto;
+    // }
   }
   // 给频道下边沿横向设置样式
   /deep/ .van-tabs__line {
