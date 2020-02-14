cd562436244a2dda0d4d13753b90fe3d8ae0a847
shane <2206907681@qq.com>
2020-02-14 15:59:21 +0800

    更多操作结构搭建

--- src/components/com-moreaction.vue
+++ src/components/com-moreaction.vue
@@ -1,10 +1,31 @@
 <template>
   <div>
-      <!-- 通过:value接收父组件的值，通过input事件把值传递给父组件，此处van标签已经做过处理 -->
-      <!-- 如果是普通标签，@input="$emit('input',$event.target.value)" -->
-      <!--  -->
-    <van-dialog :value="value" @input="$emit('input',$event)" :show-confirm-button="false" close-on-click-overlay>
-        更多内容
+    <!-- 通过:value接收父组件的值，通过input事件把值传递给父组件，此处van标签已经做过处理 -->
+    <!-- 如果是普通标签，@input="$emit('input',$event.target.value)" -->
+    <!--  -->
+    <van-dialog
+      :value="value"
+      @input="$emit('input',$event)"
+      :show-confirm-button="false"
+      close-on-click-overlay
+    >
+      <van-cell-group v-if="isOneLevel">
+        <van-cell icon="location-o" title="不感兴趣" />
+        <van-cell icon="location-o" title="举报垃圾内容" is-link @click="isOneLevel=false" />
+        <van-cell icon="location-o" title="屏蔽作者" />
+      </van-cell-group>
+      <van-cell-group v-else>
+        <van-cell icon="arrow-left" @click="isOneLevel=true" />
+        <van-cell title="其他问题" icon="location-o" />
+        <van-cell title="标题夸张" icon="location-o" />
+        <van-cell title="低俗色情" icon="location-o" />
+        <van-cell title="错别字多" icon="location-o" />
+        <van-cell title="旧闻重复" icon="location-o" />
+        <van-cell title="广告软文" icon="location-o" />
+        <van-cell title="内容不实" icon="location-o" />
+        <van-cell title="涉嫌违法犯罪" icon="location-o" />
+        <van-cell title="侵权" icon="location-o" />
+      </van-cell-group>
     </van-dialog>
   </div>
 </template>
@@ -17,12 +38,12 @@ export default {
     value: {
       type: Boolean,
       default: false
-
     }
   },
   data () {
     return {
-      show: false
+      show: false,
+      isOneLevel: true
     }
   }
 }
