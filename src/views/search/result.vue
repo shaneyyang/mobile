6e9948d5fec6e58b9b21a0d8cd081b1cde6c69b9
shane <2206907681@qq.com>
2020-02-18 19:44:32 +0800

    搜索数据渲染

--- src/views/search/result.vue
+++ src/views/search/result.vue
@@ -3,9 +3,12 @@
     <van-nav-bar title="搜索结果" left-arrow @click-left="$router.back()" />
        <!-- 瀑布流加载 -->
     <van-list v-model="loading" :finished="finished" finished-text="没有更多了"
-              @load="onLoad">
-      <van-cell v-for="item in list" :key="item" :title="item"/>
-    </van-list>
+          @load="onLoad">
+  <van-cell
+            v-for="item in searchList"
+            :key="item.art_id.toString()"
+            :title="item.title"/>
+</van-list>
   </div>
 </template>
 
@@ -18,9 +21,10 @@ export default {
     return {
       searchList: [], // 搜索结果
       // 瀑布相关
-      list: [],
       loading: false,
-      finished: false
+      finished: false,
+      page: 1, // 页码
+      per_page: 10 // 条数
     }
   },
   computed: {
@@ -31,32 +35,45 @@ export default {
     }
   },
   created () {
-    // 调用搜索方法
-    this.getSearchList()
   },
   methods: {
   // 瀑布加载
-    onLoad () {
-    // 异步更新数据
-    // setTimeout 仅做示例，真实场景中一般为 ajax 请求
-      setTimeout(() => {
-        for (let i = 0; i < 10; i++) {
-          this.list.push(this.list.length + 1)
-        }
+    // async onLoad () {
+    // // 异步更新数据
+    // // setTimeout 仅做示例，真实场景中一般为 ajax 请求
+    // // 加载延迟效果
+    //   await this.$sleep(800)
+    //   // 获取数据
+    //   const result = await apiSearchList({ q: this.q, page: this.page, per_page: this.per_page })
+    //   // 加载状态结束
+    //   this.loading = false
 
-        // 加载状态结束
-        this.loading = false
+    //   if (!result.results.length) {
+    //     // 数据全部加载完成
+    //     this.finished = true
+    //     return false
+    //   }
+    //   // 追加数据
+    //   // this.searchList.push(...result.results)
+    //   // 页码累加
+    //   this.page++
+    // }
+    async onLoad () {
+      await this.$sleep(1000) // 暂停1s，不要太着急加载
 
+      const result = await apiSearchList({ q: this.q, page: this.page, per_page: this.per_page })
+      // 加载状态结束
+      this.loading = false
+
+      if (!result.results.length) {
         // 数据全部加载完成
-        if (this.list.length >= 40) {
-          this.finished = true
-        }
-      }, 1000)
-    },
-    // 获得搜索结果
-    async getSearchList () {
-      const result = await apiSearchList({ q: this.q })
-      this.searchList = result.results
+        this.finished = true
+        return false
+      }
+      // 追加数据
+      this.searchList.push(...result.results)
+      // 页码累加
+      this.page++
     }
   }
 }
