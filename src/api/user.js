8ca89d4e13be8ce2f390ee01cc2e80f4421c6e6b
shane <2206907681@qq.com>
2020-02-21 22:23:59 +0800

    生日+修改头像

--- src/api/user.js
+++ src/api/user.js
@@ -59,3 +59,16 @@ export function apiUserProfile () {
     method: 'get'
   })
 }
+/**
+ * 上传用户头像
+ * @param {FormData对象，拥有photo成员，代表被上传头像的文件对象} fdObj
+ * FormData是html5发布的技术
+ * 可以实现普通表单域信息 和 上传文件域信息的收集工作
+ */
+export function apiUserPhoto (fdObj) {
+  return request({
+    url: '/app/v1_0/user/photo', // 编辑头像的地址
+    method: 'patch', // 设置头像的类型
+    data: fdObj
+  })
+}
--- src/views/user/profile.vue
+++ src/views/user/profile.vue
@@ -20,19 +20,20 @@
           fit="cover"
           round
           :src="userProfile.photo"
+
         />
       </van-cell>
       <!-- value=xxx 设置单元格右侧内容部分 -->
       <van-cell is-link v-model.trim="userProfile.name" title="昵称" @click="showName=true" />
-      <van-cell is-link :value="userProfile.gender===0?'男':'女'" title="性别" @click="showSex=true"/>
-      <van-cell is-link v-model="userProfile.birthday" title="生日" />
+      <van-cell is-link :value="userProfile.gender===0?'男':'女'" title="性别" @click="showSex=true" />
+      <van-cell is-link v-model="userProfile.birthday" title="生日" @click="showBirthday=true" />
     </van-cell-group>
     <!-- 弹头像 -->
     <van-popup v-model="showPhoto" position="bottom">
       <!-- 内容 -->
       <!-- 1 本地相册选择图片 -->
       <!-- 2 拍照 -->
-      <van-cell is-link title="本地相册选择图片"></van-cell>
+      <van-cell is-link title="本地相册选择图片" @click="$refs.mypic.click()"></van-cell>
       <van-cell is-link title="拍照"></van-cell>
     </van-popup>
     <!-- 弹昵称 -->
@@ -51,12 +52,34 @@ v-model：是否显示
 cancel-text="取消" 展示取消按钮
     -->
     <van-action-sheet v-model="showSex" @select="onSelect" :actions="actions" cancel-text="取消"></van-action-sheet>
+    <!-- 弹生日 -->
+    <van-popup v-model="showBirthday" position="bottom">
+      <!-- 时间选择器
+v-model：默认激活显示时间
+type 时间类型，年月日
+:min-date="minDate" 允许选取的最小时间
+@cancel 单击取消按钮回调事件
+@confirm 确认选取时间回调
+      -->
+      <van-datetime-picker
+        v-model="nowDate"
+        type="date"
+        :min-date="minDate"
+        @cancel="showBirthday=false"
+        @confirm="confirmDate"
+      ></van-datetime-picker>
+    </van-popup>
+    <!-- 选择文件控件 -->
+    <input ref="mypic" @change="startUpload()" type="file" style="display:none" />
   </div>
 </template>
 
 <script>
+// 引入dayjs
+import dayjs from 'dayjs'
+
 // 获取用户资料的api
-import { apiUserProfile } from '../../api/user'
+import { apiUserProfile, apiUserPhoto } from '../../api/user'
 
 export default {
   name: 'user-profile',
@@ -66,13 +89,32 @@ export default {
       showPhoto: false, // 头像弹出层
       showName: false, // 昵称弹出层
       showSex: false, // 性别弹出层
-      actions: [{ name: '男' }, { name: '女' }] // 弹出菜单项目定义
+      actions: [{ name: '男' }, { name: '女' }], // 弹出菜单项目定义
+      showBirthday: false, // 时间弹出层
+      nowDate: new Date(), // 当前时间
+      minDate: new Date('1900-01-01') // 允许选取的最小时间
     }
   },
   created () {
     this.getUserProfile() // 调用获取用户资料的方法
   },
   methods: {
+    // 修改头像
+    async startUpload () {
+      const fd = new FormData()
+      fd.append('photo', this.$refs.mypic.files[0])
+
+      const result = await apiUserPhoto(fd)
+
+      this.userProfile.photo = result.photo
+
+      this.showPhoto = false
+    },
+    // 修改时间
+    confirmDate (val) {
+      this.userProfile.birthday = dayjs(val).format('YYYY-MM-DD')
+      this.showBirthday = false
+    },
     // 修改性别
     onSelect (val) {
       this.userProfile.gender = val.name === '男' ? 0 : 1
@@ -82,6 +124,8 @@ export default {
     // 获取用户资料的方法
     async getUserProfile () {
       this.userProfile = await apiUserProfile()
+      // 获取用户资料的生日时间
+      this.nowDate = new Date(this.userProfile.birthday)
     },
 
     save () {
