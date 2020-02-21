61047413452e51e85729de6e45f3dc3f577d8ca6
shane <2206907681@qq.com>
2020-02-21 21:12:33 +0800

    头像+昵称+性别结构绘制

--- src/views/user/profile.vue
+++ src/views/user/profile.vue
@@ -8,7 +8,7 @@
       @click-right="save()"
     ></van-nav-bar>
     <van-cell-group>
-      <van-cell is-link title="头像" center>
+      <van-cell is-link title="头像" center @click="showPhoto=true">
         <!--
         slot="default" 自定义单元格右侧内容
         fit="cover"  图片填充模式 保持宽高缩放图片，使图片的短边能完全显示出来，裁剪长边
@@ -23,10 +23,34 @@
         />
       </van-cell>
       <!-- value=xxx 设置单元格右侧内容部分 -->
-      <van-cell is-link v-model="userProfile.name" title="名称" />
-      <van-cell is-link v-model="userProfile.gender" title="性别" />
+      <van-cell is-link v-model.trim="userProfile.name" title="昵称" @click="showName=true" />
+      <van-cell is-link :value="userProfile.gender===0?'男':'女'" title="性别" @click="showSex=true"/>
       <van-cell is-link v-model="userProfile.birthday" title="生日" />
     </van-cell-group>
+    <!-- 弹头像 -->
+    <van-popup v-model="showPhoto" position="bottom">
+      <!-- 内容 -->
+      <!-- 1 本地相册选择图片 -->
+      <!-- 2 拍照 -->
+      <van-cell is-link title="本地相册选择图片"></van-cell>
+      <van-cell is-link title="拍照"></van-cell>
+    </van-popup>
+    <!-- 弹昵称 -->
+    <van-popup v-model="showName" position="bottom">
+      <!-- 编辑用户昵称  双向绑定用户的昵称-->
+      <van-field v-model.trim="userProfile.name" type="textarea" rows="3"></van-field>
+    </van-popup>
+    <!-- 弹性别，
+van-action-sheet"菜单"弹出层组件
+与普通弹出层的区别就是，只可以设置"菜单"项目，
+而普通弹出层可以自定义展示内容
+
+v-model：是否显示
+@select：选取项目的回调处理(内部可以设置弹层关闭、还可以设置选中项目信息等)
+:actions：可选取项目菜单信息设定 [{ name: '男' }, { name: '女' }]
+cancel-text="取消" 展示取消按钮
+    -->
+    <van-action-sheet v-model="showSex" @select="onSelect" :actions="actions" cancel-text="取消"></van-action-sheet>
   </div>
 </template>
 
@@ -38,14 +62,23 @@ export default {
   name: 'user-profile',
   data () {
     return {
-      // 用户信息
-      userProfile: {}
+      userProfile: {}, // 用户信息
+      showPhoto: false, // 头像弹出层
+      showName: false, // 昵称弹出层
+      showSex: false, // 性别弹出层
+      actions: [{ name: '男' }, { name: '女' }] // 弹出菜单项目定义
     }
   },
   created () {
     this.getUserProfile() // 调用获取用户资料的方法
   },
   methods: {
+    // 修改性别
+    onSelect (val) {
+      this.userProfile.gender = val.name === '男' ? 0 : 1
+      // 关闭弹层
+      this.showSex = false
+    },
     // 获取用户资料的方法
     async getUserProfile () {
       this.userProfile = await apiUserProfile()
